ec1a409548601cca045c60bb48bff7be5453e17d
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-08 08:46:01 +0700

    up

--- Nodejs/src/app.js
+++ Nodejs/src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
