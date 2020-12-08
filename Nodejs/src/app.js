3a52d2d272e7139fbf355f251fe44f7502f9166a
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-08 08:43:41 +0700

    up

--- Nodejs/src/app.js
+++ Nodejs/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 app.use(express.static(__dirname + "/css/style.css"));
