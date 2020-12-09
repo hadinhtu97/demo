d1093e37b2264ab649b2baeb102373a2f15100c9
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-09 15:19:49 +0700

    add css file

--- Nodejs-Express/app.js
+++ Nodejs-Express/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
     console.log('Your app is listening on port ' + port);
