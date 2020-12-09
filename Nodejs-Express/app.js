9d964a644afc2482a2ff71260418619e21433b37
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-09 15:23:29 +0700

    add env file, package dotenv and  bodyparser

--- Nodejs-Express/app.js
+++ Nodejs-Express/app.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
 app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
