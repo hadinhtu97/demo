344375dc01b4c0e0691bbb74688d1a8a6e231a75
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-15 20:30:30 +0700

    remove mongodb, mongoose, add port variable to env

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
 const app = express();
-const port = 3000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
 
-app.listen(port, () => {
-    console.log('Your app is listening on port ' + port);
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Your app is listening on port ' + process.env.PORT);
 })
