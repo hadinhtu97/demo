70ce2bc106bf0e2576d7d933119ba6d2857196b6
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-02 11:22:18 +0700

    up

---
+++ demoNumPy.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# print(np.__version__)
+
+# arrList = np.array([1, 2, 3, 4, 5])
+# arrTurpe = np.array((1, 2, 3, 4, 5))
+# arrSet = np.array({1, 2, 3, 4, 5})
+
+# print(arrList)
+# print(type(arrList))
+# print(arrTurpe)
+# print(type(arrTurpe))
+# print(arrSet)
+# print(type(arrSet))
+
+# arr = np.array([1, 2, 3], ndmin=3)
+# print(arr)
+# print(arr.ndim)
+
+# arr = np.array([[1, 2, 3], [3, 4, 5]])
+# print(arr[0][0])
+# print(arr[0, 0])
+
+# arr = np.array([1, 'a', 3, 4, 5])
+# print(arr.dtype)
+
+# arr = np.array([1, 2, 3])
+# newArr = arr.copy()
+# arr[1] = 14
+# print(arr)
+# print(newArr)
+
+# arr = np.array([1, 2, 3])
+# newArr = arr.view()
+# arr[1] = 14
+# print(arr)
+# print(newArr)
+
+arr = np.array([1, 2, 3])
+print(arr.shape)
