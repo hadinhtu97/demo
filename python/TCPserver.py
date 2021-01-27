d1b8cccfff5880fb954a42dda4516954d44dd224
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-28 02:51:31 +0700

    update

--- python/TCPserver.py
+++ python/TCPserver.py
@@ -1,10 +1,11 @@
 import socket
 
-# create a server socket by call socket function, define socket family and socket type
+# create a server socket object by call socket function, define socket family and socket type
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # define host and port
-host = socket.gethostbyname()
+host = '192.183.9.102'
+# host = socket.gethostbyname()
 port = 444
 
 # bind host and port to server socket
@@ -13,14 +14,17 @@ serversocket.bind((host, port))
 # set up TCP listener, listen for max connections is 3
 serversocket.listen(3)
 
+# keep server always listen
 while True:
+    print('Server is listening!')
     # when the client connect to server
     clientsocket, adress = serversocket.accept()
 
     print('Received connection from: ' + str(adress))
 
     # Send data back to the client
-    clientsocket.send('Connection to the server success!')
+    message = 'Connection to the server success!'
+    clientsocket.send(message.encode('ascii'))
 
     # close client connection
     clientsocket.close()
